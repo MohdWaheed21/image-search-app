ddf44a8a06065f4a4133488337007bb7092ac8d6
Mohd Waheed <mohmmadshaikwaheed@gmail.com>
2025-06-10 11:49:20 +0530

    ui

--- app.py
+++ app.py
@@ -127,6 +127,4 @@ def search_images():
     return jsonify({"results": results})
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True)
