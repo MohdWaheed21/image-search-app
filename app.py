f53d3fe3d0f6d0cb20f72ab6685c5f54f10fc087
Mohd Waheed <mohmmadshaikwaheed@gmail.com>
2025-06-10 11:37:26 +0530

    trying to deploy

--- app.py
+++ app.py
@@ -127,4 +127,6 @@ def search_images():
     return jsonify({"results": results})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
